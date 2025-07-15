0298868425f37a8b2b0aad61afac010942ae93a0
Bhuvi0312 <bhuvanasangari24@gmail.com>
2025-07-15 15:12:47 +0530

    Update sentiment_analysis_app.py

--- sentiment_analysis_app.py
+++ sentiment_analysis_app.py
@@ -13,11 +13,49 @@ import streamlit as st
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
 
-model = BertForSequenceClassification.from_pretrained("sentiment_model")
-tokenizer = BertTokenizer.from_pretrained("sentiment_model")
+st.set_page_config(page_title="Sentiment Analysis", page_icon="https://static.vecteezy.com/system/resources/previews/026/226/868/non_2x/sentiment-analysis-icon-illustration-vector.jpg", layout="centered")
+
+st.markdown(
+    """
+    <style>
+        body {
+            background-color: #f0f2f6;
+        }
+        .stApp {
+            background-image: linear-gradient(to right, #ffffff, #e0f7fa);
+            color: #000000;
+            padding: 20px;
+            font-family: 'Segoe UI', sans-serif;
+        }
+        .stTextInput>div>div>input {
+            background-color: #ffffff;
+            color: #000000;
+        }
+        .stButton>button {
+            background-color: #4CAF50;
+            color: white;
+            font-weight: bold;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    """
+    <div class="title-container">
+        <img src="https://static.vecteezy.com/system/resources/previews/026/226/868/non_2x/sentiment-analysis-icon-illustration-vector.jpg" alt="Logo"  width="30" style="margin-right: 15px;">
+        <h1>Real-Time Sentiment Analysis App</h1>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+model = BertForSequenceClassification.from_pretrained("C:\\Users\\bhuva\\Downloads\\sentiment_model")
+tokenizer = BertTokenizer.from_pretrained("C:\\Users\\bhuva\\Downloads\\sentiment_model")
 model.eval()
 
-st.title("💬 Real-Time Sentiment Analysis App")
+st.title("💬Real-Time Sentiment Analysis App")
 
 user_input = st.text_area("Enter your feedback:")
 
@@ -28,3 +66,4 @@ if st.button("Predict") and user_input.strip():
         prediction = torch.argmax(outputs.logits, dim=1).item()
         label_map = {0: "Negative", 1: "Positive", 2: "Neutral"}
         st.write(f"Sentiment: **{label_map.get(prediction, 'Unknown')}**")
+        
